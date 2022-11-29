0f24036a1e500fa282018c44fa01aff5fbe77a7a
berkay.cayli <berkay.cayli@defacto.com>
2022-11-29 10:46:27 +0300

    add optional weather service

---
+++ lib/weather/application/weather_service.dart
@@ -0,0 +1,24 @@
+import 'package:weather_bloc/weather/data/repositories/weather_repository.dart';
+import 'package:weather_bloc/weather/domain/weather.dart';
+
+// * [repository] den gelen veri, bir takım işlemlerden geçecekse
+// * yani, business logic içeriyorsa
+// * ilgili business logic, [controller] katmanında değil [service] katmanında olmalıdır
+// * [controller] 'ın görevi, ilgili [service] fonksiyonlarını çağırıp,
+// * [view] ı güncellemekten ibarettir
+
+// * [service] katmanı opsiyoneldir.
+// * iş mantığı içermeyen durumlarda, kullanılmasına gerek yoktur
+
+class WeatherService {
+  final IWeatherRepository weatherRepository;
+
+  WeatherService(this.weatherRepository);
+
+  Future<List<Weather>> getWeathers(bool fromCache) {
+    if (fromCache) {
+      return weatherRepository.getWeathers();
+    }
+    return weatherRepository.getWeathers();
+  }
+}
